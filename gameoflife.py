d19dd0c13af6bc6f76d875f3e4e834d6faeb7a0d
songfish <songyu_727@163.com>
2019-06-12 19:47:57 +0800

    add T

--- gameoflife.py
+++ gameoflife.py
@@ -1,29 +1,34 @@
 import numpy as np
-import matplotlib as plt
-
 class GameofLife():
-    def game(self, matrix):
-        matrix_shape = matrix.shape
+
+    def evolve(self, m):
+        neigh = m[0, 0] + m[0, 1] + m[0, 2] + m[1, 0] + m[1, 2] + m[2, 0] + m[2, 1] + m[2, 2]
+        if neigh == 3:
+            center = 1
+        elif neigh == 2:
+            center = m[1, 1]
+        else:
+            center = 0
+        return center
+
+    def oneStep(self, matrix):
+        matrix_shape = np.array(matrix).shape
         expand_matrix = np.pad(matrix, ((1, 1), (1, 1)), 'constant')
-
-        def evolve(m):
-            neigh = m[0, 0] + m[0, 1] + m[0, 2] + m[1, 0] + m[1, 2] + m[2, 0] + m[2, 1] + m[2, 2]
-            if neigh == 3:
-                center = 1
-            elif neigh == 2:
-                center = m[1, 1]
-            else:
-                center = 0
-            return center
-
-        finalresult = []
+        oneStepResult = []
         for i in range(1, matrix_shape[0]+1):
             row = []
             for j in range(1, matrix_shape[1]+1):
                 new_cell = expand_matrix[i-1:i+2, j-1:j+2]
-                center = evolve(new_cell)
+                center = self.evolve(new_cell)
                 row.append(center)
-            finalresult.append(row)
+            oneStepResult.append(row)
+        return oneStepResult
+
+    def game(self, matrix, T):
+        finalresult = []
+        for t in range(T):
+            matrix = self.oneStep(matrix)
+            finalresult.append(matrix)
         return finalresult
 
 
@@ -36,5 +41,6 @@ matrix[2, 0] = 1
 matrix[2, 2] = 1
 matrix[3, 1] = 1
 matrix[3, 2] = 1
-result = s.game(matrix)
+T = 20
+result = s.game(matrix, T)
 print(np.array(result))
